ed4a753cedc1df0ef8551bed2f1ffb265c3ad860
kiruba33 <108586777+kirubasankar007@users.noreply.github.com>
2025-07-30 15:48:32 +0530

    Time: 2 ms (55.15%) | Memory: 8 MB (4.16%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -5,16 +5,16 @@ public:
     int guessNumber(int n) {
         int left = 1, right = n;
         while (left <= right) {
-            int mid = left + (right - left) / 2;  // prevent overflow
+            int mid = left + (right - left) / 2;
             int res = guess(mid);
 
             if (res == 0) 
-                return mid;        // found the pick
+                return mid;        
             else if (res < 0) 
-                right = mid - 1;   // pick is lower
+                right = mid - 1;   
             else 
-                left = mid + 1;    // pick is higher
+                left = mid + 1;  
         }
-        return -1;  // should never reach here
+        return -1; 
     }
 };
