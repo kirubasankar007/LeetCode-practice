40094519028d020e78a7d8bcf34821a159195dc0
kiruba33 <108586777+kirubasankar007@users.noreply.github.com>
2026-01-19 17:17:31 +0530

    Time: 8 ms (96.16%) | Memory: 44 MB (91.27%) - LeetSync

--- 837-most-common-word/most-common-word.java
+++ 837-most-common-word/most-common-word.java
@@ -3,36 +3,41 @@ import java.util.*;
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
 
+        paragraph = paragraph.toLowerCase();
+        paragraph = paragraph.replace('!', ' ')
+                             .replace('?', ' ')
+                             .replace('\'', ' ')
+                             .replace(',', ' ')
+                             .replace(';', ' ')
+                             .replace('.', ' ');
+
+
         Set<String> bannedSet = new HashSet<>();
         for (String b : banned) {
-            bannedSet.add(b.toLowerCase());
+            bannedSet.add(b);
         }
 
-       
-        String[] words = paragraph
-                .toLowerCase()
-                .replaceAll("[^a-z ]", " ")
-                .split("\\s+");
+        String[] words = paragraph.split(" ");
+
+        Map<String, Integer> map = new HashMap<>();
 
        
-        Map<String, Integer> countMap = new HashMap<>();
-
-        for (String word : words) {
-            if (!bannedSet.contains(word) && word.length() > 0) {
-                countMap.put(word, countMap.getOrDefault(word, 0) + 1);
+        for (String w : words) {
+            if (w.length() > 0 && !bannedSet.contains(w)) {
+                map.put(w, map.getOrDefault(w, 0) + 1);
             }
         }
 
-        String answer = "";
+        String ans = "";
         int max = 0;
 
-        for (String key : countMap.keySet()) {
-            if (countMap.get(key) > max) {
-                max = countMap.get(key);
-                answer = key;
+        for (String key : map.keySet()) {
+            if (map.get(key) > max) {
+                max = map.get(key);
+                ans = key;
             }
         }
 
-        return answer;
+        return ans;
     }
 }
